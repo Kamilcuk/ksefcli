dbc1d5098fc21db8eb95a23fe626aca3caefc201
Kamil Cukrowski <kamilcukrowski@gmail.com>
2026-02-04 13:04:34 +0100

    Add --no-tokencache option to disable token caching

--- src/KSeFCli/IWithConfigCommand.cs
+++ src/KSeFCli/IWithConfigCommand.cs
@@ -29,7 +29,8 @@ public abstract class IWithConfigCommand : IGlobalCommand
     [Option("cache", HelpText = "Active profile name")]
     public string TokenCache { get; set; } = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile), ".cache", "ksefcli", "ksefcli.json");
 
-
+    [Option("no-tokencache", HelpText = "Disable token cache usage")]
+    public bool NoTokenCache { get; set; }
 
     private readonly Lazy<ProfileConfig> _cachedProfile;
     private readonly Lazy<KsefCliConfig> _cachedConfig;
@@ -225,6 +226,13 @@ public abstract class IWithConfigCommand : IGlobalCommand
 
     public async Task<string> GetAccessToken(CancellationToken cancellationToken)
     {
+        if (NoTokenCache)
+        {
+            Log.LogInformation("Token cache disabled, starting new auth");
+            AuthenticationOperationStatusResponse response = await Auth(cancellationToken).ConfigureAwait(false);
+            return response.AccessToken.Token;
+        }
+
         TokenStore tokenStore = GetTokenStore();
         TokenStore.Key key = GetTokenStoreKey();
         TokenStore.Data? storedToken = tokenStore.GetToken(key);
