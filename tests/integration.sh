263d24022dfb293b764fabbb43666b143eec79c8
Kamil Cukrowski <kamilcukrowski@gmail.com>
2026-02-15 21:08:10 +0100

    remove jq dependency

--- tests/integration.sh
+++ tests/integration.sh
@@ -16,19 +16,19 @@ clitest_z_integration_SprawdzLimitCertyfikatow() {
 	setup_integration_config || return 0
 	local output
 	L_unittest_cmd -v output cli SprawdzLimitCertyfikatow -a mytoken
-	jq -es . <<<"$output" >/dev/null || return 1
+	"$DIR"/jq_sed.sh - check <<<"$output" >/dev/null || return 1
 }
 
 clitest_z_integration_PobierzFaktury() {
 	setup_integration_config || return 0
 	#
 	L_unittest_cmd -v output cli SzukajFaktur -a token2 -v --from 2026-01-21T00:00:00+01:00 --to 2026-01-22T00:00:00+01:00
-	L_unittest_cmd -I -r '[12]' jq length <<<"$output"
+	L_unittest_cmd -I -r '[12]' "$DIR"/jq_sed.sh - length <<<"$output"
 	#
 	L_with_cd_tmpdir
 	L_unittest_cmd cli PobierzFaktury -a token2 -v --from 2026-01-21T00:00:00+01:00 --to 2026-01-22T00:00:00+01:00 -o . --pdf
 	L_unittest_cmd ls -lah 5260215591-20260124-01006068A46A-59.{json,pdf,xml}
-	L_unittest_cmd -v _ jq . 5260215591-20260124-01006068A46A-59.json
+	L_unittest_cmd -v _ "$DIR"/jq_sed.sh 5260215591-20260124-01006068A46A-59.json check
 }
 
 clitest_z_integration_PrzeslijFaktury() {
